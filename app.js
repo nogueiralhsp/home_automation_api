b4255b36d765aa2cb646f2eaf5e9e234740d6e63
Luiz Henrique Pinto <nogueiralhsp@gmail.com>
2023-04-11 21:19:42 +0100

    replaced error with json reply

--- app.js
+++ app.js
@@ -31,19 +31,25 @@ app.use(usersRouter);
 app.use(testAPI)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+
+  // res.render('error'); this was commented out to return only json as bellow
+    res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 module.exports = app;
